d00106b4e97abdcce6fa6bb33f837a55329d5476
al <al@LAPTOP-G9H4CJJM>
2019-04-18 14:52:47 -0400

    testing

---
+++ sits/src/sits/loop.java
@@ -0,0 +1,20 @@
+package sits;
+
+public class loop {
+
+	public static void main(String[] args) 
+	// main method start
+	{
+		// TODO Auto-generated method stub
+		
+		// this loop will display odd number from 1 to 10
+		int x;
+		for ( x=1; x>0; x= x+2)
+		{
+			System.out.println(x);
+		}
+		
+
+	 } // main method end
+
+}
