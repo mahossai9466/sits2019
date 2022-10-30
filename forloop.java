4b712c048cdfe9c92e281681fcae8b3ddc5f6070
mahossai9466 <44586515+mahossai9466@users.noreply.github.com>
2022-10-30 14:35:22 -0400

    Add files via upload
     new code

---
+++ forloop.java
@@ -0,0 +1,21 @@
+package sits1;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		
+		int n;
+		for ( n=1; n >0;  n=n+1)
+			
+		{
+			
+			System.out.println(n);
+			
+		}
+		
+		
+	
+
+	}
+
+}
